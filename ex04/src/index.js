0e6ea520fec071a4056b786ee8c6c95e76bc6e9b
TA <TA@TAs-iMac.local>
2021-09-17 23:40:33 +0200

    JavaScript project ex04

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,7 +1,7 @@
 function main(){
     //Only change code below this line
 
- var myString = "This is the first string. " + "This is the second string."
+ var myString = "This is the first string. " + "This is the second string.";
 
     //Only change code above this line
     return myString;   
